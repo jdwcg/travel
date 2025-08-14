9d3d375b128423e66ac6aaf0393fcd7e37f14a6e
조대운 <bigcloudjo@gmail.com>
2025-08-14 11:28:37 +0900

    요일 에러 수정

--- src/data/travelDates.ts
+++ src/data/travelDates.ts
@@ -19,7 +19,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day1',
         date: '1',
-        day: '화',
+        day: '수',
         type: 'activity',
         content: '여행 준비 및 짐 싸기',
         contentType: 'table',
@@ -53,7 +53,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day2',
         date: '2',
-        day: '수',
+        day: '목',
         type: 'hotel',
         lodging: 'hotel',
         content: '제주도 도착 및 호텔(난타제주) 체크인',
@@ -61,7 +61,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day3',
         date: '3',
-        day: '목',
+        day: '금',
         type: 'camping',
         lodging: 'camping',
         content: '제주올레캠핑장 체크인(자리 12번)',
@@ -69,7 +69,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day4',
         date: '4',
-        day: '금',
+        day: '토',
         type: 'camping',
         lodging: 'camping',
         content: '캠핑장에서 바비큐 및 주변 산책',
@@ -77,7 +77,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day5',
         date: '5',
-        day: '토',
+        day: '일',
         type: 'camping',
         lodging: 'camping',
         content: '현지 맛집 브런치 및 자유 일정',
@@ -85,7 +85,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day6',
         date: '6',
-        day: '일',
+        day: '월',
         type: 'camping',
         lodging: 'camping',
         content: '해변 캠핑 및 근교 드라이브',
@@ -93,7 +93,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day7',
         date: '7',
-        day: '월',
+        day: '화',
         type: 'camping',
         lodging: 'camping',
         content: '근교 관광 및 캠핑 정리',
@@ -101,7 +101,7 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day8',
         date: '8',
-        day: '화',
+        day: '수',
         type: 'hotel',
         lodging: 'hotel',
         content: '캠핑장 체크아웃 / 완도(루미아 호텔) 체크인',
@@ -109,14 +109,14 @@ export const travelDates: TravelItem[] = [
     {
         id: 'day9',
         date: '9',
-        day: '수',
+        day: '목',
         type: 'activity',
         content: '귀가 준비 및 안전운전',
     },
     {
         id: 'day10',
         date: '10',
-        day: '목',
+        day: '금',
         type: 'activity',
         content: '남은 휴가',
     },
